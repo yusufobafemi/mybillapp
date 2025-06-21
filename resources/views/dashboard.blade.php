56f9f30cc3997654c9d4512c57a3d5cb01667a11
yusufobafemi <yusufkb.developer@gmail.com>
2025-06-21 23:23:51 +0100

    quick update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -116,11 +116,10 @@
                     </thead>
                     <tbody>
                         @forelse($transactions as $transaction)
-                            {{ dd($transaction) }}
                             <x-transaction-row 
                                 :date="$transaction->created_at->format('d M Y')" 
                                 :time="$transaction->created_at->format('h:i A')" 
-                                :transactionId="$transaction->reference ?? 'TXN' . str_pad($transaction->id, 6, '0', STR_PAD_LEFT)" --}}
+                                :transactionId="strtoupper($transaction->reference)"
                                 :icon="getTransactionIcon($transaction->transaction_type_id)"
                                 :iconClass="strtolower(getTransactionTypeName($transaction->transaction_type_id))" 
                                 :primaryText="$transaction->description ?? 'Transaction'" 
